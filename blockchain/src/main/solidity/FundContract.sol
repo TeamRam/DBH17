d1000827f242b0777eaa6e5967bf62f12a15b426
Chris de Vreeze <chris.de.vreeze@ebpi.nl>
2017-02-11 09:06:13 +0100

    Started working on FundContract

--- blockchain/src/main/solidity/FundContract.sol
+++ blockchain/src/main/solidity/FundContract.sol
@@ -1,4 +1,20 @@
 pragma solidity ^0.4.0;
 
 contract FundContract {
+    
+    struct Participant {
+        address participantAddress;
+        uint balance;
+    }
+    
+    mapping(address => Participant) public participants;
+    
+    // Invariant: total balance of this contract must equal sum of balances of participants
+    
+    function deposit(address participant) payable {
+        participants[msg.sender] = Participant({
+            participantAddress: msg.sender,
+            balance: msg.value
+        });
+    }
 }
